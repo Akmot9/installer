8356f340d2efaa27782e3987a6c8d9fb87205e63
Cyprien AVICO <95435335+Akmot9@users.noreply.github.com>
2023-03-26 19:03:03 +0200

    Update Jenkinsfile.groovy
    groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,29 +4,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                // Clone your code repository here
-                // Install any necessary dependencies
-            }
-        }
-
-        stage('Compile') {
-            steps {
-                sh '''
-                    # Install PyInstaller
-                    pip install pyinstaller
-
-                    # Compile your Python script using PyInstaller
-                    pyinstaller --onefile installer.py
-                '''
-            }
-        }
-
-        stage('Test') {
-            steps {
-                sh '''
-                    # Run the compiled executable
-                    dist/installer
-                '''
+                sh 'pyinstaller --onefile installer.py'
             }
         }
     }
