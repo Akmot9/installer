221ef04d0bc9f440c5c9b5daa4bf181df865bb8e
Cyprien AVICO <95435335+Akmot9@users.noreply.github.com>
2023-03-26 19:35:37 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,7 +13,7 @@ pipeline {
                 withCredentials([usernamePassword(credentialsId: 'd6ec43f6-17ef-4301-a800-f3367e26cbc4', usernameVariable: 'GITHUB_USERNAME', passwordVariable: 'GITHUB_PASSWORD')]) {
                     sh '''
                         git config --global user.email "avicocyprien@yahoo.fr"
-                        git config --global user.name "akmot9"
+                        git config --global user.name "Akmot9"
                         git add .
                         git commit -m "Automated build"
                         git push https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/akmot9/installer.git HEAD:main
