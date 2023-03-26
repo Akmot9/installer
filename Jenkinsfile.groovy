a39ea977fc96891b3fc2909d23326f40ef35f096
Cyprien AVICO <95435335+Akmot9@users.noreply.github.com>
2023-03-26 20:21:37 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,14 +10,14 @@ pipeline {
         
         stage('Publish to GitHub') {
             steps {
-                withCredentials([string(credentialsId: '051e60a0-4a9b-4f9d-bfff-fc15b059b2e3', variable: 'MY_TOKEN')]) {
-                    sh '''
-                        git config --global user.email "avicocyprien@yahoo.fr"
-                        git config --global user.name "Akmot9"
-                        git add .
-                        git commit -m "Automated build"
-                        git push https://USERNAME:${MY_TOKEN}@github.com/akmot9/installer.git HEAD:main
-                    '''
+                
+                sh '''
+                    git config --global user.email "avicocyprien@yahoo.fr"
+                    git config --global user.name "Akmot9"
+                    git add .
+                    git commit -m "Automated build"
+                    git push https://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@github.com/akmot9/installer.git HEAD:main
+                '''
                 }
             }
         }
