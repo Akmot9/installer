3f609abd544b7cd173387c9b684118b8c205e126
Cyprien AVICO <95435335+Akmot9@users.noreply.github.com>
2023-03-26 19:16:12 +0200

    Update Jenkinsfile.groovy
    groovy push git

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,24 @@
 pipeline {
     agent any
-
+    
     stages {
         stage('Build') {
             steps {
                 sh 'pyinstaller --onefile installer.py'
+            }
+        }
+        
+        stage('Publish to GitHub') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'github-creds', usernameVariable: 'GITHUB_USERNAME', passwordVariable: 'GITHUB_PASSWORD')]) {
+                    sh '''
+                        git config --global user.email "avicocyprien@yahoo.fr"
+                        git config --global user.name "akmot9"
+                        git add .
+                        git commit -m "Automated build"
+                        git push https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/akmot9/installer.git HEAD:main
+                    '''
+                }
             }
         }
     }
